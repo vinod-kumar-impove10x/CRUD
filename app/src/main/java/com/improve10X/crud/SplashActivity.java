056c8f93f78753e8e5f50a199d957ef31656ceca
Vinodkumar M <vinod.kumar@improve10x.com>
2022-12-05 11:19:27 +0530

    Created SplashActivity & Developed.

---
+++ app/src/main/java/com/improve10X/crud/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.improve10X.crud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+
+        Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            Intent intent = new Intent(this,DashboardActivity.class);
+            startActivity(intent);
+             finish();
+        }, 5000);
+    }
+}
