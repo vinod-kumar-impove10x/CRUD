484a85af1d71a476cfebbad0d7844f858e26c8a6
Vinodkumar M <vinod.kumar@improve10x.com>
2022-12-15 01:03:25 +0530

    Added dummy data for testing.

--- app/src/main/java/com/improve10X/crud/quotes/Quote.java
+++ app/src/main/java/com/improve10X/crud/quotes/Quote.java
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName;
 public class Quote {
     @SerializedName("_id")
     String id;
-    String quotesText;
+    String quoteText;
     String category;
     String authorName;
     String imageUrl;
--- app/src/main/java/com/improve10X/crud/quotes/QuotesActivity.java
+++ app/src/main/java/com/improve10X/crud/quotes/QuotesActivity.java
@@ -7,12 +7,37 @@ import android.os.Bundle;
 import com.improve10X.crud.R;
 import com.improve10X.crud.base.BaseActivity;
 
+import java.util.ArrayList;
+
 public class QuotesActivity extends BaseActivity {
+
+    private ArrayList<Quote> quotes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quotes);
         getSupportActionBar().setTitle("Quotes");
+        setupData();
+    }
+
+    private void setupData() {
+        quotes = new ArrayList<>();
+
+        Quote quote1 = new Quote();
+        quote1.id = "6397f97daaf0eb03e8f948d3";
+        quote1.quoteText = "Your FUTURE is created by what you do TODAY";
+        quote1.category = "career";
+        quote1.authorName = "";
+        quote1.imageUrl = "https://www.kochiesbusinessbuilders.com.au/wp-content/uploads/2022/02/motivational-quote.jpg";
+        quotes.add(quote1);
+
+        Quote quote2 = new Quote();
+        quote2.id = "6397f97daaf0eb03e8f948d3";
+        quote2.quoteText = "Your FUTURE is created by what you do TODAY";
+        quote2.category = "career";
+        quote2.authorName = "";
+        quote2.imageUrl = "https://www.kochiesbusinessbuilders.com.au/wp-content/uploads/2022/02/motivational-quote.jpg";
+        quotes.add(quote2);
     }
 }
